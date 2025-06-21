c817bf80ce983cb04054ec4735991350cfcb453f
Matias Pacini <matiaspacini052@gmail.com>
2025-06-21 16:03:52 -0300

    A

--- unla-startup/src/components/NavBar.jsx
+++ unla-startup/src/components/NavBar.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // Si quieres usar los iconos de lucide-react, debes instalarlos:
 // npm install lucide-react
-import { Home, Plus, LogIn } from 'lucide-react';
+import { Home, Plus, LogIn, User } from 'lucide-react';
 
 function Navbar() {
   return (
     <nav className="sidebar">
       <Link to="/"><Home size={24} /><span>INICIO</span></Link>
-      <Link to="/createpost"><Plus size={24} /><span>POST</span></Link>
-      <Link to="/login"><LogIn size={24} /><span>LOGIN</span></Link>
+      <Link to="/"><Plus size={24} /><span>PROYECTO</span></Link>
+      <Link to="/"><User size={24} /><span>PERFIL</span></Link>
+      <Link to="/"><LogIn size={24} /><span>LOGIN</span></Link>
     </nav>
   );
 }
